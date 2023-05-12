467da3d2185b29583017669da2b4747abd7119dd
soaza <chukimguan@gmail.com>
2023-05-12 07:56:17 +0100

    travel: add day 3

--- travels/Europe/figure.jsx
+++ travels/Europe/figure.jsx
@@ -4,7 +4,13 @@ import useBaseUrl from "@docusaurus/useBaseUrl";
 
 export default function Figure({ src, caption }) {
   return (
-    <figure style={{ border: "0px solid #888", padding: 20 }}>
+    <figure
+      style={{
+        border: "0px solid #888",
+        padding: 20,
+        margin: "auto",
+      }}
+    >
       <img src={useBaseUrl(src)} alt={caption} />
       <figcaption
         style={{ textAlign: "center", color: "gray" }}
